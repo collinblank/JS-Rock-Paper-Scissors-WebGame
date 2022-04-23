1a341465565df1639d1f2d397f3cdd03b5721ac1
Collin Blank <collin@Collins-Mac-mini.local>
2022-04-23 11:04:13 -0400

    Added Logic for Lizard and Spock

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+//creating array and logic for moves to correspond with correct win/loss/tie situations. Track who won and lost
 const selectionButtons = document.querySelectorAll('[data-selection]')
 const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
 const selections = [
   {
     name: 'rock',
     emoji: '🪨',
-    beats: 'scissors'
+    beats: ['scissors', 'lizard'],
   },
   {
     name: 'paper',
     emoji: '📄',
-    beats: 'rock'
+    beats: ['rock' , 'spock'],
   },
   {
     name: 'scissors',
     emoji: '✂️',
-    beats: 'paper'
-  }
+    beats: ['paper', 'lizard'],
+  },
+  {
+    name: 'lizard',
+    emoji: '🦎',
+    beats: ['paper', 'spock'],
+  },
+  {
+    name: 'spock',
+    emoji: '🖖',
+    beats: ['rock', 'scissors'],
+  },
+
 ]
 
 selectionButtons.forEach(selectionButton => {
@@ -32,6 +46,13 @@ function makeSelection(selection) {
   const computerWinner = determineWinner(computerSelection, selection)
   addSelectionResult(computerSelection, computerWinner)
   addSelectionResult(selection, personWinner)
+
+  if (personWinner) incrementScore(yourScoreSpan)
+  if (computerWinner) incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
 }
 
 function addSelectionResult(selection, winner) {
@@ -43,7 +64,7 @@ function addSelectionResult(selection, winner) {
 }
 
 function determineWinner(selection, opponentSelection) {
-  return selection.beats === opponentSelection.name
+  return selection.beats[0] === opponentSelection.name || selection.beats[1] === opponentSelection.name
 }
 
 function randomSelection() {
